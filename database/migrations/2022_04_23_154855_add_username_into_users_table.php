fe6ba844990709181b0a473b2e2266accefc5c22
Zhangqi Chen <zhangqi.chen@rightcapital.com>
2022-04-24 00:02:44 +0800

    init
    Add an `username` column into users

---
+++ database/migrations/2022_04_23_154855_add_username_into_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->string('username')
+                ->nullable()
+                ->comment('用户名称')
+                ->after('email')
+                ->index('idx_username');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+            $table->dropColumn('username');
+        });
+    }
+};
